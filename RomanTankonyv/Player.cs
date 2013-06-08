f1cfe7806ad3e47a332f23ac467c5526185cff3c
unknown <geryjuhasz@gmail.com>
2013-06-08 23:55:38 +0300

    commiting project

---
+++ RomanTankonyv/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Media;
+using WMPLib;
+using System.IO;
+using System.Resources;
+
+namespace RomanTankonyv
+{
+    class Player
+    {
+        public void playSimpleSound(string media)
+        {
+            WindowsMediaPlayer mp = new WindowsMediaPlayer();
+            byte[] b = (byte[]) Properties.Resources.ResourceManager.GetObject(media);
+            FileInfo fileInfo = new FileInfo("play.mp3");
+            FileStream fs = fileInfo.OpenWrite();
+            fs.Write(b, 0, b.Length);
+            fs.Close();
+            mp.URL = fileInfo.Name;
+            mp.controls.play();
+        }
+    }
+}
